fa56c6e5738e4db6f07045d08eb60d7190d2bf1b
ANDRE <klai1@mail.ru>
2022-08-02 14:52:54 +0400

    Сделал 4 задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worl");
+
+        //Задание 1,2
+        //int[] weight=new int[3];
+        //weight[0]=1;
+        //weight[1]=2;
+        //weight[2] = 3;
+
+       // float[] weight2={1.57f, 7.654f, 9.986f};
+       // int[] weight3=new int[4];
+        //weight3[0]=10;
+        //weight3[1]=13;
+       // weight3[2]=15;
+       // weight3[3]=17;
+
+        //System.out.print(weight[0]+",");
+       // System.out.print(weight[1]+",");
+       // System.out.print(weight[2]);
+        System.out.println();
+       // System.out.print(weight2[0]+", ");
+       // System.out.print(weight2[1]+", ");
+        //System.out.print(weight2[2]);
+        System.out.println();
+        //System.out.print(weight3[0]+", ");
+        //System.out.print(weight3[1]+", ");
+        //System.out.print(weight3[2]+", ");
+       // System.out.print(weight3[3]);
+
+        //Задание 3
+       // int[] weight=new int[3];
+       // weight[0]=1;
+       // weight[1]=2;
+       // weight[2] = 3;
+
+        float[] weight2={1.57f, 7.654f, 9.986f};
+        int[] weight3=new int[4];
+        weight3[0]=10;
+        weight3[1]=13;
+        weight3[2]=15;
+        weight3[3]=17;
+
+        //System.out.print(weight[2]+",");
+       // System.out.print(weight[1]+",");
+       // System.out.print(weight[0]);
+        System.out.println();
+        System.out.print(weight2[2]+", ");
+        System.out.print(weight2[1]+", ");
+        System.out.print(weight2[0]);
+        System.out.println();
+        System.out.print(weight3[3]+", ");
+        System.out.print(weight3[2]+", ");
+        System.out.print(weight3[1]+", ");
+        System.out.print(weight3[0]);
+        System.out.println();
+
+        //Задание 4
+        int[] weight=new int[3];
+        weight[0]=1;
+        weight[1]=2;
+        weight[2] =3;
+        for (int i = 0; i < weight.length; i++) {
+            System.out.print(++weight[i]+", ");
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
